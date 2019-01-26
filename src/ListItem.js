2f0e7b7b43b14f26cb29c8c68badfa6d891c9735
Hao <haoteng10@gmail.com>
2019-01-26 02:28:29 +0000

    Restore color to the commas

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
+import Parser from 'html-react-parser';
 
 class ListItem extends Component {
     render(){
         
-        const webDevelopment = {
+        var webDevelopment = {
             title: "Web Development",
             cssClasses: "Web-Development display-grid",
             content: "I have experiences in full-stack web development.",
             special: "<strong> Areas of Study includes: HTML, CSS, Javascript, Node.Js. </strong>",
-            studies: ["HTML", "CSS", "Javascript", "Node.js", "React"]
+            studies: ["HTML", "CSS", "Javascript", "Node.js", "React"],
+            // studies: ["HTML sfdkjsldfsdfkl", " sdfsdf CSS", "Javascript", "Node.js", "React"],
+            processedStudies: ""
         };
         
         const minecraftDevelopment = {
@@ -16,8 +19,12 @@ class ListItem extends Component {
             cssClasses: "Minecraft display-grid",
             content: "I have experiences in developing simple Minecraft plugins that will have its designed functions and features by interacting with the Spigot API.",
             special: "<strong> Areas of Study includes: Java </strong>",
-            studies: ["Java"]
+            studies: ["Java"],
+            processedStudies: ""
         };
+        
+        webDevelopment.studies = ["HTML", "CSS", "Javascript", "Node.js", "React"];
+        webDevelopment.processedStudies = "";
         
         const information = [webDevelopment, minecraftDevelopment];
         
@@ -26,34 +33,34 @@ class ListItem extends Component {
         const style = {fontWeight: 'bold'};
         
         function processStudies (inputArray){
-            inputArray.processedStudies = inputArray.studies.map((value, i) => {
+            inputArray.studies.forEach((value, i) => {
                if (i !== inputArray.studies.length -1){
-                   return value + ", ";
+                    inputArray.processedStudies += value + "<span class='text-primary'>, </span>";
                } else {
-                   return value;
+                   return inputArray.processedStudies += value;
                }
             });
         }
         
-        const processed = information.map((currentArray, i) => {
+        information.forEach((currentArray, i) => {
             processStudies(currentArray);
-            return 1;
             // console.log(currentArray.processedStudies);
         });
         
-        
+        // webDevelopment.processedStudies = "<strong>Hi</strong>";
         
         return (
             
             information.map((h, i) => {
                 
-                return <div key={i} className= {h.cssClasses}>
+                return <div key={i*2} className= {h.cssClasses}>
                          <div className={basicContainerStyle}>
                               <h2>{h.title}</h2>
                               <p>{h.content}</p>
                               <div className={"card text-primary bg-light mb-3"}>
                                       <div className={"card-body"}>
-                                              <p className={"card-text"} style={style}>Areas of study includes: <span style={{color: 'deepskyblue'}}>{h.processedStudies}</span></p>
+                                              <p style={{color: 'blue'}}>{h.processedStudies}</p>
+                                              <p className={"card-text"} style={style}>Areas of study includes: <span style={{color: "deepskyblue"}}>{Parser(h.processedStudies)}</span></p>
                                       </div>
                               </div>
                           </div>
